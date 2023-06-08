c0fc40a9fb410386ca1b28e8b4c8589090dba92d
Shelley Chen <shelleychen318@gmail.com>
2023-06-07 20:38:46 -0400

    added lab 4 p1

---
+++ lab4/p1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// function that takes a natural number n and print all binary string of length n in ascending order
+void printNBinary(int n, string str="") {
+    // base case: we are at last character of binary string
+    if (n == 0) {
+        cout << str << endl;
+    }
+    // not at last character of binary string
+    else {
+        // recursive calls to append next value to binary string
+        printNBinary(n - 1, str + "0"); 
+        printNBinary(n - 1, str + "1");
+    }
+};
+
+int main () {
+    printNBinary(4);
+    return 0;
+}
